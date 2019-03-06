2004e9ac7b91efba412f39d351a222dcce2f265f
Jose Arandia Luna <jose.arandia.luna@mail.com>
2019-03-06 07:00:54 -0400

    Added cors support

--- src/main/java/com/jmamoon/todolist/controllers/appuser/AppUserRestController.java
+++ src/main/java/com/jmamoon/todolist/controllers/appuser/AppUserRestController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Jose Arandia Luna https://github.com/jma-moon
  */
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class AppUserRestController {
